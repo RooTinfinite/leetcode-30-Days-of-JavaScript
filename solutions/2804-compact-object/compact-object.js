c6e93bb7d572fe67703571d81de3c4f87329a3b2
Piotr M <filo74b@gmail.com>
2024-11-14 18:16:34 +0100

    Time: 79 ms (76.60%) | Memory: 59.2 MB (83.10%) - LeetSync

---
+++ solutions/2804-compact-object/compact-object.js
@@ -0,0 +1,27 @@
+function compactObject(obj) {
+
+  if (Array.isArray(obj)) {
+    return obj.filter(Boolean).map(val => {
+        if (typeof val === 'object') {
+          return compactObject(val);
+        }
+        return val;
+      })
+     
+  }
+
+  for (const [key,val] of Object.entries(obj)) {
+
+    if (!val) {
+      delete obj[key];
+      continue;
+    }
+    
+    if (typeof val === 'object') {
+      obj[key] = compactObject(val);
+    }
+  }
+
+  return obj;
+
+}

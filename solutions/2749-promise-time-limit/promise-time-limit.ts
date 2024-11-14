5541caf822c92639b64352205b8008067a00fc7e
Piotr M <filo74b@gmail.com>
2024-11-14 18:05:31 +0100

    Time: 68 ms (36.90%) | Memory: 51.6 MB (50.15%) - LeetSync

---
+++ solutions/2749-promise-time-limit/promise-time-limit.ts
@@ -0,0 +1,11 @@
+type Fn = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn, t: number): Fn {
+  return async function(...args) {
+    const timeLimitPromise = new Promise((resolve, reject) => {
+      setTimeout(() => reject("Time Limit Exceeded"), t)
+    });
+    const returnedPromise = fn(...args);
+    return Promise.race([timeLimitPromise, returnedPromise]);
+  }
+};

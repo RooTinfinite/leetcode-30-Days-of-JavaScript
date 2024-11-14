d9ae891edb1f95c02ebf8dc401a39138324ea440
Piotr M <filo74b@gmail.com>
2024-11-14 18:01:49 +0100

    Time: 84 ms (6.78%) | Memory: 49.9 MB (8.18%) - LeetSync

--- solutions/2859-add-two-promises/add-two-promises.js
+++ solutions/2859-add-two-promises/add-two-promises.js
@@ -1,7 +1,4 @@
 var addTwoPromises = async function(promise1, promise2) {
-  // Wait for both promises to resolve and retrieve their values
   const [value1, value2] = await Promise.all([promise1, promise2]);
-
-  // Return a new promise that resolves with the sum of the values
   return value1 + value2;
 };

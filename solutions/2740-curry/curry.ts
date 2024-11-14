a3e4f4ab14d5e4e2b3fc2945ed3d4f27dd4ad5d8
Piotr M <filo74b@gmail.com>
2024-11-14 18:25:13 +0100

    Time: 59 ms (82.61%) | Memory: 56.4 MB (16.92%) - LeetSync

---
+++ solutions/2740-curry/curry.ts
@@ -0,0 +1,10 @@
+function curry(fn: Function): Function {
+  return function curried(...args: any[]) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+    return function (...innerArgs: any[]) {
+      return curried.apply(this, args.concat(innerArgs));
+    };
+  };
+};
